d88da0b2e384770756b5c0856e5b41abf4ca639a
ahuh <ahuh@outlook.fr>
2024-11-19 14:08:36 +0100

    FIX tab name

--- scripts/_wsl-compact.bat
+++ scripts/_wsl-compact.bat
@@ -1,3 +1,3 @@
 @echo off
 call %~dp0include\common.bat %1
-"%CEL_CONEMU_PATH%" -reuse -run pwsh.exe -c "wslcompact -c -y %CEL_WSL_DISTRIB%" -new_console:%CEL_SPLIT_ARG%:c:P:"%CEL_CONEMU_PWSH_PALETTE%":t:"WSL Update"
+"%CEL_CONEMU_PATH%" -reuse -run pwsh.exe -c "wslcompact -c -y %CEL_WSL_DISTRIB%" -new_console:%CEL_SPLIT_ARG%:c:P:"%CEL_CONEMU_PWSH_PALETTE%":t:"WSL Compact"
